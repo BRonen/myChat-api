0647f4c97fc58a939763c9895159da714f46e878
BRonen <brennopereira6@gmail.com>
2019-12-20 04:42:03 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+   //res.json({'server': 'running'});
+   res.sendFile(path.join(__dirname + '/../public/index.html'));
+});
+
+var clients = 0;
+io.on('connection', function(socket) {
+   clients++;
+   console.log(clients);
+
+   socket.on('disconnect', function () {
+      clients--;
+      console.log(clients);
+   });
+
+   socket.on('send', (data) => {
+      console.log(`${data.author}: ${data.text}`);
+      io.sockets.send({'text': data.text+'<br />', 'author':data.author});
+   });
+   socket.emit('test', clients);
+});
+
+server.listen(process.env.PORT || 5000, function() {
+   console.log('listening on *:5000');
+});
