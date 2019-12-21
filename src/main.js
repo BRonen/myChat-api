49cf214ecea9cd843bb9ac14e8b00cbf87afb6d4
BRonen <brennopereira6@gmail.com>
2019-12-21 02:56:35 -0300

    react client bug fixed

--- src/main.js
+++ src/main.js
@@ -8,10 +8,11 @@ const io = require('socket.io')(server);
 app.use(express.json());
 app.use(cors());
 
-app.get('/', function(req, res) {
-   //res.json({'server': 'running'});
+app.get( '/', function(req, res) {
    res.sendFile(path.join(__dirname + '/../public/index.html'));
 });
+
+app.get( '/test', () => res.json({'server': 'running'}) );
 
 var clients = 0;
 io.on('connection', function(socket) {
@@ -21,13 +22,14 @@ io.on('connection', function(socket) {
    socket.on('disconnect', function () {
       clients--;
       console.log(clients);
+      socket.broadcast.emit('test', clients);
    });
 
    socket.on('send', (data) => {
       console.log(`${data.author}: ${data.text}`);
-      io.sockets.send({'text': data.text+'<br />', 'author':data.author});
+      io.sockets.send({'text': data.text, 'author':data.author});
    });
-   socket.emit('test', clients);
+   socket.broadcast.emit('test', clients);
 });
 
 server.listen(process.env.PORT || 5000, function() {
